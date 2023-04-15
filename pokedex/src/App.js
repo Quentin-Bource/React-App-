842b71e68b862cd81fa44d7683df562df614cb4e
Quentin <quentin01.bource@gmail.com>
2023-04-15 15:36:18 +0200

    Beggin2.0

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Pokemon from "./pages/Pokemon";
+import About from "./pages/About";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/pokémon" element={<Pokemon />} />
         <Route path="*" element={<Home />} />
       </Routes>
---
+++ pokedex/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quos iste incidunt repellat ea explicabo at odio, nobis accusamus eum veritatis, doloremque officiis eaque quidem perspiciatis expedita assumenda tenetur? Aperiam impedit saepe architecto ab doloribus placeat expedita consequuntur. Nulla pariatur dignissimos laboriosam amet voluptatibus repudiandae sequi velit culpa, vero laudantium iste nemo modi aliquid porro molestias architecto enim blanditiis? Fuga cum vitae reprehenderit atque architecto aliquid consequuntur iste. Totam fuga facilis temporibus commodi excepturi exercitationem aut atque maxime eius. Aperiam vitae quisquam quaerat eveniet, repellendus dolorum. Vitae corrupti minima fugit similique totam harum beatae libero magnam tempore, voluptate suscipit quia?</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At illo voluptatum reprehenderit aliquam tempore corrupti deserunt, quaerat ipsum omnis quae dolor corporis sunt debitis quasi quos repudiandae doloribus, ipsa laboriosam.</p>
+        </div>
+    );
+};
+
+export default About;
