c8938676ea0d2c6701e0886e00aae1cc0da696f1
Quentin <quentin01.bource@gmail.com>
2023-04-28 10:02:55 +0200

    lastversion

--- pokedex/src/pages/Pokemon.js
+++ pokedex/src/pages/Pokemon.js
@@ -8,19 +8,32 @@ import { NavLink } from 'react-router-dom';
 const Pokemon = () => {
 
     const [pokemon, setPokemon] = useState([]);
+    const [selectedRadio, setSelectedRadio] = useState("")
+    const radios = [1 , 2];
+    
 
     useEffect(() => {
         axios.get("https://api-pokemon-fr.vercel.app/api/v1/pokemon").then((res) => {
-            const filteredPokemon = res.data.filter((poke) => poke.pokedexId <= 151 && poke.pokedexId > 0);
+            const filteredPokemon = res.data.filter((poke) => poke.pokedexId <= 165 && poke.pokedexId > 0);
             setPokemon(filteredPokemon);
         });
     }, []);
     return (
         <div>
             <Navigation />
+
+                {radios.map((gen) => (
+                    <li>
+                        <input type="radio" id={gen} name='genfiltre' onChange={(e) => setSelectedRadio(e.target.id)} />
+                        <label htmlFor={gen}>Génération {gen}</label>
+
+                    </li>
+                ))}
             
                 <div className='allCard'>
-                    {pokemon.map((poke) => (
+                    {pokemon
+                    // .filter((poke) => poke.generation[0].includes(selectedRadio))
+                    .map((poke) => (
                         <NavLink key={poke.pokedexId} to={`/fiche/${poke.pokedexId}`}>
                         <Card key={poke.pokedexId} poke={poke} />
                         </NavLink>
